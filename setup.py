c42fa970bed456f387b8accd40cc0440f10201df
He Zhao <james_littlefox@hotmail.com>
2013-05-31 17:25:20 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='grooveshark',
+        version='0.1',
+        description='Simple python library of Grooveshark Public API v3',
+        author='He Zhao',
+        author_email='james_littlefox@hotmail.com',
+        url='https://github.com/hezhao/Grooveshark',
+        packages=find_packages()
+    )
